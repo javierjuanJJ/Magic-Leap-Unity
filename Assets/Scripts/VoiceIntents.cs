6ed973ab9f62363df9092e397b4ffa17931519b3
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-11-01 17:54:14 +0100

    Voice - Handle permissions

---
+++ Assets/Scripts/VoiceIntents.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class VoiceIntents : MonoBehaviour
+{
+    private readonly MLPermissions.Callbacks permissionCallbacks = new MLPermissions.Callbacks();   
+
+    // subscribe to permission events
+    private void Awake()
+    {
+        permissionCallbacks.OnPermissionGranted += OnPermissionGranted;
+        permissionCallbacks.OnPermissionDenied += OnPermissionDenied;
+        permissionCallbacks.OnPermissionDeniedAndDontAskAgain += OnPermissionDenied;
+    }
+
+    // unsubscribe from permission events
+    private void OnDestroy()
+    {
+        permissionCallbacks.OnPermissionGranted -= OnPermissionGranted;
+        permissionCallbacks.OnPermissionDenied -= OnPermissionDenied;
+        permissionCallbacks.OnPermissionDeniedAndDontAskAgain -= OnPermissionDenied;
+    }
+
+    // request permission for voice input at start
+    private void Start()
+    {
+        MLPermissions.RequestPermission(MLPermission.VoiceInput, permissionCallbacks);
+    }
+
+    // on voice permission denied, disable script
+    private void OnPermissionDenied(string permission)
+    {
+        Debug.LogError($"Failed to initialize voice intents due to missing or denied {MLPermission.VoiceInput} permission. Please add to manifest. Disabling script.");
+        enabled = false;
+    }
+
+    // on voice permission granted, initialize voice input
+    private void OnPermissionGranted(string permission)
+    {
+        if (permission == MLPermission.VoiceInput)
+            InitializeVoiceInput();
+    }
+
+
+    // check if voice commands setting is enabled, then set up voice intents
+    private void InitializeVoiceInput()
+    {
+        bool isVoiceEnabled = MLVoice.VoiceEnabled;
+
+        // if voice setting is enabled, try to set up voice intents
+        if (isVoiceEnabled)
+        {
+            Debug.Log("Voice commands setting is enabled");
+        }
+
+        // if voice setting is disabled, open voice settings so user can enable it
+        else
+        {
+            Debug.Log("Voice commands setting is disabled - opening settings");
+            UnityEngine.XR.MagicLeap.SettingsIntentsLauncher.LaunchSystemVoiceInputSettings();
+            Application.Quit();
+        }
+    }
+}
